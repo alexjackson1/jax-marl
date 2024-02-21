c97b2e7e276337f0ff67cce75091ce8986a86e1c
Alex Jackson <mail@alexjackson.uk>
2024-02-21 03:47:01 +0000

    Start to work with actions

--- jaxmarl/environments/traitors/logic.py
+++ jaxmarl/environments/traitors/logic.py
@@ -11,12 +11,11 @@ class GameLogic:
 
     @staticmethod
     def run_breakfast(
-        key: chex.PRNGKey, state: "State", actions: Dict[str, chex.Array]
+        _key: chex.PRNGKey, state: "State", actions: Dict[int, chex.Array]
     ) -> "State":
-        LENGTH = 2
         assert state.activity == Activity.BREAKFAST
         assert state.phase == BreakfastPhase.GROUP_DISCUSSION
-        assert state.phase_step <= LENGTH
+        assert state.phase_step <= state.config.open_length
 
         if state.phase_step == 1:
             # logger.heading_2("Breakfast")
@@ -24,10 +23,26 @@ class GameLogic:
             state = state.reset_shields()
             state = state.discover_death()
 
-        # TODO
-        self.group_discussion()
+        GameLogic.group_discussion(actions)
 
         state = state.step()
+
+    @staticmethod
+    def group_discussion(
+        state: "State", actions: Dict[int, chex.Array]
+    ) -> Dict[int, chex.Array]:
+        # TODO: this is really wrong atm
+        for id, action in actions.items():
+            assert action.shape == ()
+
+            idx = 1 + 2 + 2 + 1 + state.config.num_agents
+            idx_2 = idx + state.config.num_agents * state.config.num_symbols
+            if action >= idx and action < idx_2:
+                # valid, update
+                pass
+            else:
+                # invalid, ignore
+                pass
 
     @staticmethod
     def run_challenge(
